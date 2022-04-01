e53fafa2cc1dc3cf7527443b931fa04e2c9d1b6c
arynchoong <arynchoong@hotmail.com>
2022-04-02 06:45:52 +0800

    Notebook for Chapter  7.

---
+++ notebooks/20220402/promotions.py
@@ -0,0 +1,32 @@
+promos = []
+
+def promotion(promo_func):
+    promos.append(promo_func)
+    return promo_func
+
+@promotion
+def fidelity_promo(order):
+    """5% discount for customers with 1000 or more fidelity points"""
+    return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+@promotion
+def bulk_item_promo(order):
+    """10% discount for each LineItem with 20 or more units"""
+    discount = 0
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * .1
+    return discount
+
+@promotion
+def large_order_promo(order):
+    """7% discount for orders with 10 or more distinct items"""
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * .07
+    return 0
+
+def best_promo(order):
+    """Select best discount available
+    """
+    return max(promo(order) for promo in promos)
---
+++ notebooks/20220402/registration.py
@@ -0,0 +1,27 @@
+registry = []
+
+def register(func):
+    print('running register(%s)' % func)
+    registry.append(func)
+    return func
+
+@register
+def f1():
+    print('running f1()')
+    
+@register
+def f2():
+    print('running f2()')
+
+def f3():
+    print('running f3()')
+
+def main():
+    print('running main()')
+    print('registry ->',  registry)
+    f1()
+    f2()
+    f3()
+
+if __name__=='__main__':
+    main()
